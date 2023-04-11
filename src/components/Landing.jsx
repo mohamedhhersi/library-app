624a74d5fcd6f7494484706abea1e3527cc9b5be
Mohamed <mohamed.hersi1232@gmail.com>
2023-04-11 04:27:05 -0400

    initial commit

---
+++ src/components/Landing.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import UndrawBooks from '../assets/Undraw_Books.svg'
+
+function Landing() {
+  return (
+    <section>
+        <header>
+            <div className="header__container">
+            <div className="header__description">
+             <h1>Australias most awarded online library platform</h1>
+             <h2>Find your dream book with <span className='purple'>Library</span></h2> 
+             <a href="#features">
+                <button className="btn">Browse Books</button>
+                </a>
+                </div>  
+                <figure className="header__img--wrapper">
+                <img src={UndrawBooks} alt="" />
+            </figure>
+            </div>
+            
+        </header>
+      
+    </section>
+  )
+}
+
+export default Landing
---
+++ src/components/Nav.jsx
@@ -0,0 +1,47 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import LibraryLogo from '../assets/Library.svg'
+
+function Nav() {
+  return (
+    <nav className="nav__container">
+    <a href="/">
+      <img src={LibraryLogo} alt="" className="logo"/>
+    </a>
+    <ul className="nav__links">
+      <li className="nav__list">
+        <a href="">Home</a>
+      </li>
+      <li className="nav__list">
+        <a href="">About</a>
+      </li>
+      <button className="btn__menu">
+        <FontAwesomeIcon icon = 'bars' />
+      </button>
+      <li className="nav__icon">
+        <a href="/cart" className="nav__link">
+            <FontAwesomeIcon icon='shopping-cart'/>
+        </a>
+        <span className="cart__length">2</span>
+      </li>
+    </ul>
+    <div className="menu__backdrop">
+        <button className="btn__menu btn__menu--close">
+            <FontAwesomeIcon icon='times' />
+        </button>
+        <ul className="menu__links">
+            <li className="menu__list">
+                <a href="" className="menu__link">Home</a>
+            </li>
+            <li className="menu__list">
+                <a href="" className="menu__link">Books</a>
+            </li>
+            <li className="menu__list">
+                <a href="" className="menu__link">Cart</a>
+            </li>
+        </ul>
+    </div>
+  </nav>
+  )
+}
+
+export default Nav
