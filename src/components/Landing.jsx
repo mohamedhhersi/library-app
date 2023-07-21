c15c326067376c5a403c17ef519f339d1ab46096
Mohamed Hersi <mohamed.hersi1232@gmail.com>
2023-07-21 08:33:31 -0400

    ready for deployment

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import UndrawBooks from "../assets/Undraw_Books.svg";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function Landing() {
   return (
